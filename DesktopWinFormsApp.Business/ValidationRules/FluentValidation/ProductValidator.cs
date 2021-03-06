be80c7e2411124a3b207eb45d6d43555f774d863
ArifTarp <arifoklh@gmail.com>
2021-03-06 15:08:09 +0300

    Added fluent validation

---
+++ DesktopWinFormsApp.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using DesktopWinFormsApp.Entity.Concrete;
+
+namespace DesktopWinFormsApp.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product name cannot be empty");
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.UnitsInStock).NotEmpty();
+
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitsInStock).GreaterThanOrEqualTo((short)0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 2);
+
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Product name must begin with a");
+        }
+
+        // custom rule
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
