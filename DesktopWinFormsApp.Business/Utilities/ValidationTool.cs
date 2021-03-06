3ca3442708eb415534e9a960c447b7886f21fa09
ArifTarp <arifoklh@gmail.com>
2021-03-06 15:21:38 +0300

    Added utilities

---
+++ DesktopWinFormsApp.Business/Utilities/ValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using DesktopWinFormsApp.Entity.Abstract;
+
+namespace DesktopWinFormsApp.Business.Utilities
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator iValidator, object entity)
+        {
+            var result = iValidator.Validate(entity);
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
